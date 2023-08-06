c43c9acf3c0e16eb663964fb6fb6ce375d3c93b4
asaf ben artzi <asaf.benartzi@bankleumi.co.il>
2023-08-06 10:21:45 +0300

    Status bar colors

---
+++ app/src/main/java/com/example/scoresapp/viewmodels/StoryViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.scoresapp.ui.match
+
+import androidx.lifecycle.ViewModel
+
+class StoryViewModel : ViewModel() {
+
+    var playWhenReady = true
+    var mediaItemIndex = 0
+    var playbackPosition = 0L
+    var pressTime = 0L
+
+
+
+}
